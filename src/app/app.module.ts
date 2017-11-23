9192a0a3a98f86943ff467cede5454b526b4a7d7
Hyunmo <yhmgood0130@gmail.com>
2017-11-22 20:10:35 -0700

    Login added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,29 +2,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from "@angular/forms";
 import { HttpModule } from "@angular/http";
+import { routing } from "./app.routing";
 import { MatButtonModule,MatToolbarModule, MatCheckboxModule } from '@angular/material';
+import { MatInputModule } from '@angular/material/input';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import 'hammerjs';
 
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { LoginComponent } from './components/login/login.component';
 
+import { LoginService } from './services/login.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpModule,
+    routing,
     MatButtonModule,
+    MatGridListModule,
     MatCheckboxModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatInputModule
   ],
-  providers: [],
+  providers: [
+    LoginService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
